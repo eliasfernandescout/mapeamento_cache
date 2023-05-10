f9b8acd1a2f65e7d3ea4ef2fb86e94da8c79dc8f
eliasfernandescout <eliasfernandescout@gmail.com>
2023-05-10 15:59:35 -0300

    update

--- mapeamento.py
+++ mapeamento.py
@@ -1,7 +1,23 @@
-def inicializarCache (tamanhoCache):
-    dicionario = {}
+def inicializar_cache(tamanhoCache):
+    cache = {}
     for index in range(tamanhoCache):
-        dicionario[index] = -1
-    print(dicionario)
+        cache[index] = -1
+    return cache
 
-inicializarCache(10)
+
+outputCache = inicializar_cache(10)
+
+def imprimir_cache(cache):
+    print("Posição Cache\tPosição Memória")
+    for chave, valor in cache.items():
+        print(f"{chave}\t\t\t\t\t{valor}")
+
+
+imprimir_cache(outputCache)
+
+
+def mapeamento_direto(tamanho_cache, posicao_memoria):
+    posicao_cache = posicao_memoria % tamanho_cache
+
+
+
